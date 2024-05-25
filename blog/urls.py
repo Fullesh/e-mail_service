a43854e9186c66f23f62ab491423556fd87b8545
Danila Belov <danilabelov.adm@yandex.ru>
2024-05-25 09:51:58 +0300

    Added new app. Blog

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from blog.apps import BlogConfig
+from blog.views import BlogView, BlogCreateView, BlogDetailView, BlogUpdateView, BlogDeleteView, toggle_published
+
+app_name = BlogConfig.name
+
+urlpatterns = [
+    path('', BlogView.as_view(), name='home'),
+    path('record/new/', BlogCreateView.as_view(), name='create_record'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view_record'),
+    path('edit/<int:pk>/', BlogUpdateView.as_view(), name='edit_record'),
+    path('delete/<int:pk>/', BlogDeleteView.as_view(), name='delete_record'),
+    path('published/<int:pk>/', toggle_published, name='change_published'),
+]
